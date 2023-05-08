e74c92286d5d6d4a992b83fe8103b3b139fcf85d
Keven <dake.3601@gmail.com>
2023-05-07 18:36:28 -0600

    First commit

---
+++ app.js
@@ -0,0 +1,76 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose')
+const cors = require("cors")
+require('dotenv').config()
+
+const indexRouter = require('./routes/index');
+const recordsRouter = require("./routes/records");
+
+console.log('connecting to', process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connection to MongoDB:', error.message)
+  })
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// CORS
+app.use(cors());
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/api/records', recordsRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+module.exports = app;
---
+++ models/record.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+// Record Schema 
+const recordSchema = new mongoose.Schema({
+  createDate: {
+    type: Date,
+    required: true,
+  },
+  direction: String,
+  setSpeed: mongoose.Types.Decimal128,
+  voltage: mongoose.Types.Decimal128,
+  current: mongoose.Types.Decimal128,
+  speed: mongoose.Types.Decimal128,
+});
+
+const decimal2JSON = (v, i, prev) => {
+  if (v !== null && typeof v === 'object') {
+    if (v.constructor.name === 'Decimal128')
+      prev[i] = v.toString();
+    else
+      Object.entries(v).forEach(([key, value]) => decimal2JSON(value, key, prev ? prev[i] : v));
+  }
+};
+
+recordSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+    decimal2JSON(returnedObject);
+  }
+})
+
+module.exports = mongoose.model('Record', recordSchema)
