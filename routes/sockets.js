631efcc053a8046c3706704967c06888b9c768e7
Keven <dake.3601@gmail.com>
2023-05-09 16:35:21 -0600

    ping pong to keep alive

--- routes/sockets.js
+++ routes/sockets.js
@@ -9,8 +9,16 @@ function esp32(ws, req) {
   esp32Clients.push(ws);
   console.log('ESP32 connected');
 
-  ws.on('message', function (body) {
-    body = JSON.parse(body);
+  ws.timer = setInterval(() => { ws.send("ping") }, 5000);
+
+  ws.on('message', function (msg) {
+    if (msg === "pong") return;
+    if (msg === "ping") {
+      ws.send("pong");
+      return;
+    }
+
+    const body = JSON.parse(msg);
     const record = new Record({
       timestamp: new Date(body.timestamp),
       direction: body.direction,
@@ -35,21 +43,48 @@ function esp32(ws, req) {
   ws.on('close', function () {
     esp32Clients.splice(esp32Clients.indexOf(ws), 1);
     console.log('ESP32 disconnected');
+    clearInterval(ws.timer);
   });
 }
 
 function commands(ws, req) {
+
+  ws.timer = setInterval(() => { ws.send("ping") }, 5000);
+
   ws.on('message', function (msg) {
+    if (msg === "pong") return;
+    if (msg === "ping") {
+      ws.send("pong");
+      return;
+    }
+
     esp32Clients.forEach((client) => {
       client.send(msg);
     });
   });
+
+  ws.on('close', function () {
+    clearInterval(ws.timer);
+  });
 }
 
 function updates(ws, req) {
+
+  ws.timer = setInterval(() => { ws.send("ping") }, 5000);
+
   browserClients.push(ws);
+
+  ws.on('message', function (msg) {
+    if (msg === "pong") return;
+    if (msg === "ping") {
+      ws.send("pong");
+      return;
+    }
+  });
+
   ws.on('close', function () {
     browserClients.splice(browserClients.indexOf(ws), 1);
+    clearInterval(ws.timer);
   });
 }
 
